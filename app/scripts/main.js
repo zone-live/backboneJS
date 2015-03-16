7a885c5817456929183d9cd53f9675644c3d58f5
António Regadas <antonio.regadas@premium-minds.com>
2015-03-16 17:06:32 +0000

    added a view and router

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+var UserList = Backbone.View.extend({
+	el: '.page',
+	render: function () {
+		this.$el.html('CONTENT SHOULD SHOW HERE');
+	}
+});
+
+var Router = Backbone.Router.extend({
+	routes: {
+		'': 'home'
+	}
+});
+
+var userList = new UserList();
+
+var router = new Router();
+router.on('route:home', function() {
+	userList.render();
+});
+
+Backbone.history.start();
